164ac608f98b8e7702a3948f20cbac8e13312345
Jason Ziglar <jasedit@gmail.com>
2012-02-22 20:15:22 -0700

    Refactoring script-like design to a series of functions inside a module. The Ripple.sync() function is still the single-call execution of the Ripple system.

--- lib/ripple/base.rb
+++ lib/ripple/base.rb
@@ -6,7 +6,7 @@ module Ripple
   require 'dropbox_sdk'
 
   ACCESS_TYPE = :app_folder
-  Ripple.DefaultConfiguration = {
+  DefaultConfiguration = {
     :destinationDir => './test',
     :cleanDestination => false,
     :dropbox => {
@@ -14,26 +14,85 @@ module Ripple
     }
   }
 
-  def Ripple.sync
-    conf = Ripple.DefaultConfiguration.dup 
-
-    # Load configuration.
-    if File.exists?('ripple.yaml')
-      conf = YAML::load(File.read('ripple.yaml'))
-    end
-
-    #Load Dropbox API login from file, if applicable.
-    if File.exists?('keys.yaml')
-      login = YAML::load(File.read('keys.yaml'))
+  def Ripple.connectToDropbox
+    #Load Dropbox API dropboxKeys from file, if applicable.
+    if File.exists?('dropbox_session.yaml')
+      dropboxKeys = YAML::load(File.read('dropbox_session.yaml'))
     else
+      puts "A Dropbox API key/secret is required for accessing your sync files."
+      puts "You can visit https://www.dropbox.com/developers/apps to generate these."
       puts "Please enter your Dropbox API key"
-      login['key'] = gets.chomp!
+      dropboxKeys['key'] = gets.chomp!
       puts "Please enter your Dropbox API secret"
-      login['secret'] = gets.chomp!
+      dropboxKeys['secret'] = gets.chomp!
     end
 
-    if !conf.has_key?(:destinationDir)
-      conf[:destinationDir] = './test'
+    session = nil
+
+    if dropboxKeys.has_key?(:session)
+      session = DropboxSession.deserialize(dropboxKeys[:session])
+    else
+      session = DropboxSession.new(dropboxKeys[:key], dropboxKeys[:secret])
+      session.get_request_token
+      authorize_url = session.get_authorize_url
+      puts "Visit #{authorize_url} to log in to Dropbox. Hit enter when you have done this."
+      gets
+      session.get_access_token
+    end
+
+    if session.nil?
+      return nil, nil, nil
+    end
+
+    if !dropboxKeys.has_key?(:session)
+      dropboxKeys[:session] = session.serialize()
+    end
+
+    client = DropboxClient.new(session, ACCESS_TYPE)
+
+    if client.nil?
+      return nil, nil, nil
+    else
+      return session, client, dropboxKeys
+    end
+  end
+
+  def Ripple.loadConfiguration
+    conf = Ripple::DefaultConfiguration.dup 
+
+    # Load configuration and override any values that differ from the default.
+    if File.exists?('ripple.yaml')
+      loadedConf = YAML::load(File.read('ripple.yaml'))
+      conf.merge!(loadedConf)
+    end
+
+    return conf
+  end
+
+  def Ripple.cleanup(conf, keys)
+    File.open('dropbox_session.yaml', 'w') do|file|
+      file.puts keys.to_yaml
+    end
+
+    File.open('ripple.yaml', 'w') do |file|
+      file.puts conf.to_yaml
+    end
+  end
+
+  def Ripple.sync
+    conf = Ripple.loadConfiguration()
+    session, client, dropboxKeys = Ripple.connectToDropbox()
+   
+    if session.nil?
+      puts "Could not connect to Dropbox."
+      Ripple.cleanup(conf, dropboxKeys)
+    end 
+
+    #Here we need to actually sync newest files.
+    begin
+      files = client.metadata('/', 10000, true, dropboxKeys[:files])
+    rescue DropboxNotModified
+      files = nil
     end
 
     destDir = conf[:destinationDir]
@@ -48,45 +107,6 @@ module Ripple
       end
     end
 
-    session = nil
-    if File.exists?('dropbox_session.yaml')
-      state = File.open('dropbox_session.yaml').read
-    end
-
-    if state.has_key?(:session)
-      session = DropboxSession.deserialize(state[:session])
-    else
-      session = DropboxSession.new(login[:key], login[:secret])
-      session.get_request_token
-      authorize_url = session.get_authorize_url
-      puts "Visit #{authorize_url} to log in to Dropbox. Hit enter when you have done this."
-      gets
-      session.get_access_token
-    end
-
-    if session == nil
-      exit
-    elsif !state.has_key?(:session)
-      state[:session] = session.serialize()
-    end
-
-    client = DropboxClient.new(session, ACCESS_TYPE)
-
-    #Here we need to actually sync newest files.
-    begin
-      files = client.metadata('/', 10000, true, state[:files])
-    rescue DropboxNotModified
-      files = nil
-    end
-
     print files
-
-    File.open('dropbox_session.yaml', 'w') do|file|
-      file.puts state.to_yaml
-    end
-
-    File.open('ripple.yaml', 'w') do |file|
-      file.puts conf.to_yaml
-    end
-    end
   end
+end
