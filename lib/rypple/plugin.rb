83f089522dd80c9771f3ef82139e1a1b649947ed
Jason Ziglar <jasedit@gmail.com>
2012-03-04 21:49:01 -0500

    Fixing the Plugin module to correctly create and use plugins now.
    Plugin should now be extended, not included. Also, the function
    plugin_name() is now defined for all plugins, so the name which can be
    used to create a plugin is accessible from an instance.

--- lib/rypple/plugin.rb
+++ lib/rypple/plugin.rb
@@ -3,34 +3,33 @@
 # * class. In order to enable this functionality, simply include this Module in
 # * the base class to make as a plugin.
 module Plugin
-  def self.included(base)
-    base.extend(Plugin)
-  end
+  @@subclasses = Hash.new
 
   # Register a new subclass of the class this module is included in, for later
   # creation via the create call. This allows for a plugin-like functionality.
   #
   # ==== Attributes
   # * +:name+ Name of the subclass that will be used to reference it.
-  def self.register name, &block
+  def register name, &block
     if name.empty?
       raise "Cannot create a subclass with an empty name."
     end
 
     c = Class.new(self, &block)
+    c.class_eval("def self.plugin_name() \"#{name}\" end")
 
     Object.const_set("#{name.to_s.capitalize}#{self.to_s}", c)
-    @@subclasses[name] = c.class
+    @@subclasses[name] = c
   end
 
   # The construction method to get a desired subclass based on the string
   # denoting it. This passes whatever arguments beyond the name along to the
   # initialize function.
-  def self.create name, *args
+  def create name, *args
     c = @@subclasses[name]
 
     if c
-      c.new *args
+      return c.new *args
     else
       raise "#{name} is not a valid subclass of #{c.superclass}"
     end
